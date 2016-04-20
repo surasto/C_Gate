d3c6dc4ea46166cee0c864381f0af096814aeee4
user <ralf_stoffels@gmx.de>
2016-04-20 19:20:22 +0200

    neue version

--- C_Gate_Main/lib_David.ino
+++ C_Gate_Main/lib_David.ino
@@ -1,12 +1,22 @@
 // Davids Animationsbibliothek
 
 // Davids globale Variablen
-
+unsigned long old_millis_D = 0;
 
 // Davids Funktionen
 
+
+//_____________Terror____________
+
+
 void Terror() {
-  // Hier muss Davids Code eingefügt werden 
+    if(millis() > old_millis_D +10){
+     if(wertR < 100) wertR = 255; else wertR = 0;
+     if(wertG < 100) wertG = 255; else wertG = 0;
+     if(wertB < 100) wertB = 255; else wertB = 0;
+     
+     old_millis_D = millis();
+     } 
 }
 
 
