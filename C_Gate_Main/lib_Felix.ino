8f0d589f3b335404586e874ea04dd6fd6da262c9
User-no-password <ralf_stoffels@gmx.de>
2016-04-27 19:27:03 +0200

    neue version

--- C_Gate_Main/lib_Felix.ino
+++ C_Gate_Main/lib_Felix.ino
@@ -7,29 +7,33 @@
 // Felix Funktionen
 
 void dauerGruen() {
-
-wertR=0;
-wertG=255;
-wertB=0;
+  
+  wertR=0;
+  wertG=255;
+  wertB=0;
 }
 
 void dauerBlau() {
-wertR=0;
-wertG=0;
-wertB=255;
+  wertR=0;
+  wertG=0;
+  wertB=255;
 }
 
 void dauerRot() {
-wertR=255;
-wertG=0;
-wertB=0;
+  wertR=255;
+  wertG=0;
+  wertB=0;
 }
 
 // Schaltet alle drei LEDs an
 void dauerWeiss() {
-wertR=255;
-wertG=255;
-wertB=255;
+  wertR=255;
+  wertG=255;
+  wertB=255;
 }
 
-
+void Zufall (){
+  wertR=random(0,255);
+  wertG=random(0,255);
+  wertB=random(0,255);
+}
